7cb34132ef4f415801df457343d00c5d0c59c867
Rafael Souza <rafael.souza@iexfy.com>
2023-08-26 01:42:46 -0300

    create user
    create user

---
+++ src/pages/home/styles.ts
@@ -0,0 +1,35 @@
+import { styled, Heading, Text } from '@ignite-ui/react'
+
+export const Container = styled('div', {
+  maxWidth: 'calc(100vw - (100vw - 1160px) /2)', // 100vw corresponde ao tamanho da largura total
+  marginLeft: 'auto',
+  height: '100vh',
+  display: 'flex', // elementos um ao lado outro
+  alignItems: 'center', // elementos alinhados verticalmente
+  gap: '$20', // igual a 5rem e igual a 80px
+})
+
+export const Hero = styled('div', {
+  maxWidth: 480,
+  padding: '0 $10',
+
+  [`> ${Heading}`]: {
+    '@media(max-width: 600px)': {
+      fontSize: '$6xl',
+    },
+  },
+
+  [`> ${Text}`]: {
+    marginTop: '$2',
+    color: '$gray200',
+  },
+})
+
+export const Preview = styled('div', {
+  paddingRight: '$8',
+  overflow: 'hidden',
+
+  '@media(max-width: 600px)': {
+    display: 'none',
+  },
+})
